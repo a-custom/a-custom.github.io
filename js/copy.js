0bd4462bc33b954959a3db7fee2696063824c1fa
Artur Custom <artur.verios@gmail.com>
2018-12-01 20:34:49 +0200

    Update js/copy.js

--- js/copy.js
+++ js/copy.js
@@ -1,6 +1,6 @@
 //script copyright date
 var currentTime = new Date();
-var yearoffoundation = 2017;
+var yearoffoundation = 2018;
 var currentyear = currentTime.getFullYear();
 if(yearoffoundation !== currentyear){
 	document.write(yearoffoundation + "-" + currentyear);
