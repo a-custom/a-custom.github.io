2bdab1891f0beda671465b53ca95503277440d2e
Artur Verenych <41106502+a-custom@users.noreply.github.com>
2022-09-29 12:16:29 +0300

    add CV

---
+++ index.php
@@ -0,0 +1,38 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "xhtml11.dtd">
+<html>
+<head>
+	<script data-ad-client="ca-pub-9561790517553539" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<META CONTENT="text/html; charset=utf-8" />
+	<TITLE>Сайт в разработке</TITLE>
+	<style>
+		body {margin:0;padding:0;font: 12px Tahoma;}
+		h1 {font-size:20px;color:#1F84FF;margin-bottom:20px;margin-top:0;font-weight:bolder;line-height:30px;text-align: center;}
+		#a {color:#1F84FF;}
+		div {width: 700px;margin: 100px auto 0 auto;padding-top: 50px;height: 120px;line-height: 150%;}
+		p {font-size: 16px;font-weight: bold;text-align: center;}
+		.copy {font-size: 16px;color:#1F84FF}
+	</style>
+</head>
+<body>
+<div>
+	<h1>Сайт в разработке</h1>
+	
+	<p>Приношу свои извинения, сайт находится на стадии разработки</p>
+	<p>Если Вы хотите связатся со мной, обратитесь на страницу в <a href="https://www.facebook.com/artur.veren" target="_blank">Facebook</a></p>
+</div>
+<div class="copy">
+	<p>
+		© <script type="text/javascript"?> 
+			var currentTime = new Date(); 
+			var yearoffoundation = 2020; //Год измените на свой 
+			var currentyear = currentTime.getFullYear(); 
+			if(yearoffoundation !== currentyear){ 
+				document.write(yearoffoundation + " - " + currentyear); 
+			} else { 
+				document.write(currentyear); 
+			}
+		</script> <a href="https://artur-verenych.top/">Artur Verenych</a>
+	</p>
+</div>
+</body>
+</html>
